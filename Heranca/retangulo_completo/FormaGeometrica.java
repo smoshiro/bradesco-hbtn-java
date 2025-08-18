3b2065464159278a623c39961f6870efb48262b7
Sergio Oshiro <smoshiro@gmail.com>
2025-08-18 17:12:31 -0300

    Retangulo Completo

---
+++ Heranca/retangulo_completo/FormaGeometrica.java
@@ -0,0 +1,8 @@
+public class FormaGeometrica {
+  public FormaGeometrica() {
+
+  }
+  public double area() {
+    throw new java.lang.UnsupportedOperationException("Funcao area nao implementada");
+  }
+}
---
+++ Heranca/retangulo_completo/Retangulo.java
@@ -0,0 +1,25 @@
+public class Retangulo extends FormaGeometrica {
+  private double largura;
+  private double altura;
+  public Retangulo() {
+    super();
+  }
+  public double getLargura() { return largura; }
+  public void setLargura(double largura) {
+    if (largura <= 0) throw new java.lang.IllegalArgumentException("Largura deve ser maior ou igual a 0");
+    this.largura = largura;
+  }
+  public double getAltura() { return altura; }
+  public void setAltura(double altura) {
+    if (altura <= 0) throw new java.lang.IllegalArgumentException("Altura deve ser maior ou igual a 0");
+    this.altura = altura;
+  }
+  @Override
+  public double area() {
+    return getLargura() * getAltura();
+  }
+  public String toString() {
+    return String.format("[Retangulo] %.2f / %.2f", getLargura(), getAltura());
+  }
+}
+
