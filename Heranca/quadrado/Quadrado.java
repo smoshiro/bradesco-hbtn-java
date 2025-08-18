fbaf2cd6d61d65bb8d962bc3b5285c7bdff5eff8
Sergio Oshiro <smoshiro@gmail.com>
2025-08-18 17:20:50 -0300

    Quadrado

---
+++ Heranca/quadrado/Quadrado.java
@@ -0,0 +1,14 @@
+public class Quadrado extends Retangulo {
+  private double lado;
+  public double getLado() { return lado; }
+  public void setLado(double lado) {
+    if (lado <= 0) throw new java.lang.IllegalArgumentException("Lado deve ser maior ou igual a 0");
+    this.lado = lado;
+    super.setAltura(lado);
+    super.setLargura(lado);
+  }
+  public String toString() {
+    return String.format("[Quadrado] %.2f", getLado());
+  }
+}
+
