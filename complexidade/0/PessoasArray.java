943e7be8975cddb0377c5bf8fd3fdb1ec9d89ad6
Sergio Oshiro <smoshiro@gmail.com>
2025-08-20 19:09:18 -0300

    Complexidade O(1) - Tempo Constante

---
+++ complexidade/0/PessoasArray.java
@@ -0,0 +1,26 @@
+public class PessoasArray {
+
+    private String[] nomes;
+
+    public PessoasArray() {
+        nomes =new String[]{"Amanda", "Beatriz", "Carlos","Daniela","Eduardo",
+                "Fabio","Gustavo", "Hingrid","Isabela","Joao","Leise","Maria",
+                "Norberto","Otavio","Paulo", "Quirino","Renata","Sabata",
+                "Tais","Umberto","Vanessa","Xavier"};
+    }
+
+    public String[] getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(String[] nomes) {
+        this.nomes = nomes;
+    }
+
+    // implementar o método de pesquisaConstante
+    public void pesquisaTempoConstante(int id){
+        if (id < 0 || id >= nomes.length)
+            throw new IllegalArgumentException(String.format("O array de nomes possui :%d nomes.", nomes.length));
+        System.out.printf("Nome pesquisado é %s que está na posição %d\n", nomes[id], id);
+    }
+}
