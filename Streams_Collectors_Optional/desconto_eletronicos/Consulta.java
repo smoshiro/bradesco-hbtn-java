8a31487237fed94c4b132ecfb1bdbc02e50d0524
Sergio Oshiro <smoshiro@gmail.com>
2025-08-27 16:52:05 -0300

    Eletronicos com 15% de desconto

---
+++ Streams_Collectors_Optional/desconto_eletronicos/Consulta.java
@@ -0,0 +1,55 @@
+import java.util.Comparator;
+import java.util.List;
+
+public class Consulta {
+    private Consulta() {
+        //static class
+    }
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido
+                .getProdutos()
+                .stream()
+                .filter(p -> p.getCategoria() == CategoriaProduto.LIVRO)
+                .toList()
+                ;
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos
+                .stream()
+                .max(Comparator.comparing(Produto::getPreco, Double::compareTo))
+                .orElse(null)
+                ;
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double precoMinimo) {
+        return produtos
+                .stream()
+                .filter(p -> p.getPreco() >= precoMinimo)
+                .toList()
+                ;
+    }
+
+    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedidos) {
+        return pedidos
+                .stream()
+                .filter(p -> p
+                        .getProdutos()
+                        .stream()
+                        .anyMatch(pp -> pp.getCategoria() == CategoriaProduto.ELETRONICO))
+                .toList();
+    }
+
+    public static List<Produto> aplicar15PorcentoDescontoEletronicos(List<Produto> produtos) {
+        return produtos
+                .stream()
+                .map(p -> {
+                    // deveria criar uma nova instancia...
+                    if (p.getCategoria() == CategoriaProduto.ELETRONICO) {
+                        p.setPreco(p.getPreco() * 0.85);
+                    }
+                    return p;
+                })
+                .toList();
+    }
+}
