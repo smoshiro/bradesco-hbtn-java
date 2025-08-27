ce5e2060dfc4a0ed394a2ba359ec68a113421741
Sergio Oshiro <smoshiro@gmail.com>
2025-08-27 17:39:54 -0300

    Pessoas agrupadas por cargo - fix para satisfazer checker (groupingBy na mesma linha...)

--- 0x19/pessoa_por_cargo/ConsultaPessoas.java
+++ 0x19/pessoa_por_cargo/ConsultaPessoas.java
@@ -9,8 +9,7 @@ public class ConsultaPessoas {
     public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
         return pessoas
                 .stream()
-                .collect(Collectors
-                        .groupingBy(Pessoa::getCargo))
+                .collect(Collectors.groupingBy(Pessoa::getCargo))
                 ;
     }
 }
