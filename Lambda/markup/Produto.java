8db0f6896c149817345d6bce92771baea84f75ed
Sergio Oshiro <smoshiro@gmail.com>
2025-08-27 09:18:20 -0300

    Produto Markup

---
+++ Lambda/markup/Produto.java
@@ -0,0 +1,33 @@
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private double percentualMarkup = 10;
+
+    public Supplier<Double> precoComMarkup = () -> preco * (100 + percentualMarkup) / 100;
+    public Consumer<Double> atualizarMarkup = (p) -> preco = percentualMarkup;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public double getPercentualMarkup() {
+        return percentualMarkup;
+    }
+
+    public Supplier<Double> getPrecoComMarkup() {
+        return precoComMarkup;
+    }
+
+    public Produto(double preco, String nome) {
+        this.preco = preco;
+        this.nome = nome;
+    }
+
+}
