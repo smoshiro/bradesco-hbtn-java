2c616c4e10b14fc40e753097eb27b3e66f0b50b8
Sergio Oshiro <smoshiro@gmail.com>
2025-08-27 17:38:45 -0300

    Pessoas agrupadas por cargo

---
+++ 0x19/pessoa_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+    private ConsultaPessoas() {
+        // static class
+    }
+    public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
+        return pessoas
+                .stream()
+                .collect(Collectors
+                        .groupingBy(Pessoa::getCargo))
+                ;
+    }
+}
